64409167c897fab7082ac89f753830b721bc6dd9
oskarrough <oskar@rough.dk>
2024-11-03 16:43:32 +0100

    Try auto adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-// import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-auto'
 import adapter from '@sveltejs/adapter-static'
-import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
+// import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,7 +13,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: 'index.html'
+			// fallback: 'index.html'
 		})
 	}
 }
