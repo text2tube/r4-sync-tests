61ca8ffba9a4fde32ad53dbf182abe4c09a4a0f8
Oskar Rough <oskar@rough.dk>
2025-07-08 12:14:32 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,13 @@
+import devtoolsJson from 'vite-plugin-devtools-json'
 import {sveltekit} from '@sveltejs/kit/vite'
 import {defineConfig} from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
-
+	plugins: [sveltekit(), devtoolsJson()],
 	optimizeDeps: {
 		// https://pglite.dev/docs/bundler-support#vite
 		exclude: ['@electric-sql/pglite']
 	},
-
-	worker: {
-		format: 'es'
-	},
-
-	build: {
-		target: 'esnext'
-	}
+	worker: {format: 'es'},
+	build: {target: 'esnext'}
 })
-
-// import PGWorker from './worker.js?worker'
-
-// export const pglite = new PGliteWorker(
-//   new PGWorker({
-//     type: 'module',
-//       name: 'pglite-worker',
-//     }),
-//     {
-//       // ...your options here
-//     }
-//   },
-// )
