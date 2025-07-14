bced6c12a49261062e1e9a0bce1e7b87ccbac265
Oskar Rough <oskar@rough.dk>
2025-07-14 11:19:23 +0200

    Fix loading of PG on mobile Safari

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,5 +1,5 @@
 import {browser} from '$app/environment'
-import {initDb, pg} from '$lib/db'
+import {initDb} from '$lib/db'
 import {sdk} from '@radio4000/sdk'
 
 // Disable server-side rendering for all routes by default. Otherwise we can't use pglite + indexeddb.
@@ -7,18 +7,20 @@ export const ssr = false
 
 /** @type {import('./$types').LayoutLoad} */
 export async function load() {
+	let pg = null
 	let preloading = true
 
-	try {
-		await initDb()
-	} catch (err) {
-		console.error('Failed to initialize database:', err)
-	} finally {
-		preloading = false
+	if (browser) {
+		try {
+			await initDb() // Creates PGlite instance and runs migrations
+			pg = true // Signal that pg is ready
+			window.r5 = {pg, sdk}
+		} catch (err) {
+			console.error('Failed to initialize database:', err)
+		} finally {
+			preloading = false
+		}
 	}
 
 	return {pg, sdk, preloading}
 }
-
-// @ts-expect-error just for debugging
-if (browser) window.r5 = {pg, sdk}
